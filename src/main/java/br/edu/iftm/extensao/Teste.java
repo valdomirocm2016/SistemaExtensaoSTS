8df66ca0b74cbf8c872dea6730b2e741274eaf9a
valdomirocm2016 <valdomirocm@gmail.com>
2019-02-26 14:02:06 -0300

    Campo nome adicionado na classe Teste.java

--- src/main/java/br/edu/iftm/extensao/Teste.java
+++ src/main/java/br/edu/iftm/extensao/Teste.java
@@ -1,9 +1,8 @@
 package br.edu.iftm.extensao;
 
 public class Teste {
-<<<<<<< HEAD
+
      private int idade;
-=======
      private String nome;
->>>>>>> Campo nome adicionado na classe Teste.java
+
 }
