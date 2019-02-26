7f8bea5001f43f53b4544314337e26849f65febf
valdomirocm2016 <valdomirocm@gmail.com>
2019-02-26 14:05:07 -0300

    Rebase

--- src/main/java/br/edu/iftm/extensao/Teste.java
+++ src/main/java/br/edu/iftm/extensao/Teste.java
@@ -1,5 +1,9 @@
 package br.edu.iftm.extensao;
 
 public class Teste {
+<<<<<<< HEAD
      private int idade;
+=======
+     private String nome;
+>>>>>>> Campo nome adicionado na classe Teste.java
 }
